c720299603966fc23d944cc2179c0447b0012da2
Doxca <81829364+Doxca@users.noreply.github.com>
2021-07-29 16:50:51 +0200

    Create AAAB_psteam.user.js

---
+++ AAAB_psteam.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         anti adblock
+// @version      0.2
+// @description  block ad on pstream
+// @author       Ender
+// @match        https://www.pstream.net/e/*
+// @icon         https://www.google.com/s2/favicons?domain=pstream.net
+// ==/UserScript==
+
+document.getElementById("videojs-styles").remove();
+//oui c'est tout con
