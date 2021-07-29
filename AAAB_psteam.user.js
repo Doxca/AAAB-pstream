93355499911038c02858b279be8b7230825ccfb1
Doxca <81829364+Doxca@users.noreply.github.com>
2021-07-29 16:56:30 +0200

    Update AAAB_psteam.user.js

--- AAAB_psteam.user.js
+++ AAAB_psteam.user.js
@@ -2,9 +2,13 @@
 // @name         anti adblock
 // @version      0.2
 // @description  block ad on pstream
-// @author       Ender
+// @author       Doxca
 // @match        https://www.pstream.net/e/*
 // @icon         https://www.google.com/s2/favicons?domain=pstream.net
+// @namespace       https://github.com/Doxca/AAAB-pstream
+// @updateURL       https://github.com/Doxca/AAAB-pstream/raw/main/AAAB_psteam.user.js
+// @downloadURL     https://github.com/Doxca/AAAB-pstream/raw/main/AAAB_psteam.user.js
+// @supportURL      https://github.com/Doxca/AAAB-pstream/issues
 // ==/UserScript==
 
 document.getElementById("videojs-styles").remove();
